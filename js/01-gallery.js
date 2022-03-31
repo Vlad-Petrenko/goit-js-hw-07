6a1c04eaa723ef5bc69686b21a9772ce74e518db
Vlad-Petrenko <vlad120595@gmail.com>
2022-03-31 21:43:08 +0300

    начало

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const blockGallery = document.querySelector('.gallery');
+
+const createGallary = createGalleryMarkup(galleryItems);
+
+blockGallery.insertAdjacentHTML('beforeend', createGallary);
+
+blockGallery.addEventListener('click', onBlockGalleryClick);
+
+function createGalleryMarkup(imgs) {
+  return imgs
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}" onclick="return false">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+function onBlockGalleryClick(evt) {
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  console.log(evt.target.dataset.source);
+  blockGallery.onclick = () => {
+    basicLightbox
+      .create(
+        `
+		<img width="1400" height="900" src="${evt.target.dataset.source}">
+	`
+      )
+      .show();
+  };
+}
